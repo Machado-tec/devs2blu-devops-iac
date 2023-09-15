c357eaf0a559482479bf696a97b42a526640cbda
Adriano Machado <adriano@machado.tec.br>
2023-09-14 21:56:58 -0300

    ok

--- ansible/ansbile-init.sh
+++ ansible/ansbile-init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash 
 
 # hosts.ini = inventario
-# meu_playbook.yml = playbook
+# Exemplo do hosts.ini:
+# [docker_containers]
+# 127.0.0.1 ansible_port=2222 ansible_user=root ansible_ssh_private_key_file=./id_rsa
+
+# meu_playbook.yml = install_packages.yml 
 
 ansible-playbook -i /caminho/para/o/inventario meu_playbook.yml
 
